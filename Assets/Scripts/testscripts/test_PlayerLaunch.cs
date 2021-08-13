c5c0a22a502ea6c82312ef26fe9ba4b7ff1301a8
HanGyeol <hangyeol0218@korea.ac.kr>
2021-08-13 17:46:47 +0900

    플레이어의 좌우 이동 방식 및 얼음 플랫폼 위에서 미끄러지도록 수정

---
+++ Assets/Scripts/testscripts/test_PlayerLaunch.cs
@@ -0,0 +1,205 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test_PlayerLaunch : MonoBehaviour
+{
+    public test_SerialMovement test_SerialMovement;
+
+    Camera Camera;
+    Vector2 Direction;
+    Vector2 MousePosition;
+    Rigidbody2D rbPlayer;
+    private Rigidbody2D rb;
+    public float speed, deceleration;
+    public float maxSpeed, minSpeed;
+    public bool UpDown, moved;
+    public bool stop;
+    public bool isLaunching;
+    public bool singular = false;
+
+    Vector3 colPos;
+    Vector3 colLocalScale;
+    int check;
+    PlatformSpawn spawn;
+    //public GameObject platform;
+
+    float minTime = 0, currentTime = 0, time = 0; ////수정
+    public float chargeTime = 1.5f;
+    ArrowController arrow; ////
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        test_SerialMovement = GameObject.Find("SerialMoving").GetComponent<test_SerialMovement>();
+        isLaunching = false;
+        moved = false;
+        stop = false; //플레이가 움직이는 중인지
+        UpDown = true; //up == true, Down == false
+        maxSpeed = 10;
+        speed = minSpeed = 1;
+        deceleration = 0.99f;
+        rb = GetComponent<Rigidbody2D>();
+        Camera = GameObject.Find("Main Camera").GetComponent<Camera>();
+        //platform = GameObject.Find("TestPlatform");
+        spawn = GameObject.Find("PlatformSpawner").GetComponent<PlatformSpawn>();
+        arrow = GameObject.Find("ArrowSpawner").GetComponent<ArrowController>(); ////수정
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkPlayerCount();
+        currentTime += Time.deltaTime; ////수정
+        if (moved == false && !test_SerialMovement.isJumping() && !singular)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                isLaunching = true;
+                speed = minSpeed;
+                MousePosition = Input.mousePosition;
+                MousePosition = Camera.ScreenToWorldPoint(MousePosition);
+                Direction = MousePosition - rb.position;
+                Direction = Direction.normalized;
+                minTime = currentTime; //// 수정
+                arrow.Instant(transform.position, Direction);
+                arrow.chargebarSpawn(transform.position); ////
+            }
+            if (Input.GetMouseButton(0))
+            {
+                MousePosition = Input.mousePosition; //중간에 발사위치 조정 가능하도록
+                MousePosition = Camera.ScreenToWorldPoint(MousePosition);
+                Direction = MousePosition - rb.position;
+                Direction = Direction.normalized;
+                arrow.Setting(transform.position, Direction);
+
+                if (UpDown == true) //speed 기준에서 시간 기준으로 바꿈
+                {
+                    if (currentTime - minTime >= chargeTime)
+                    {
+                        UpDown = false;
+                        minTime = currentTime = 0;
+                    }
+                }
+                else
+                {
+                    if (currentTime - minTime >= chargeTime)
+                    {
+                        UpDown = true;
+                        minTime = currentTime = 0;
+                    }
+                }////
+
+            }
+            if (Input.GetMouseButtonUp(0))
+            {
+                isLaunching = false;
+                rb.gravityScale = 0;
+                moved = true;
+                time = currentTime - minTime; ////수정
+                if (UpDown == true)
+                {
+                    speed = minSpeed + (maxSpeed - minSpeed) * (time / chargeTime);
+                }
+                else
+                {
+                    speed = maxSpeed - (maxSpeed - minSpeed) * (time / chargeTime);
+                }
+                arrow.DisInstant();
+                arrow.chargebarDestroy(); ////
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (moved == true)
+        {
+            if (speed > 0.3)
+            {
+                //rb.MovePosition(rb.position + Direction * speed * Time.fixedDeltaTime);
+                rb.velocity = Direction * speed;
+                speed *= deceleration;
+            }
+            else
+            {
+                moved = false;
+                stop = true;
+                //this.platform = GameObject.Find("TestPlatform");
+                //Instantiate(platform, transform.position, Quaternion.identity);
+                Destroy(gameObject);
+                spawn.Platform1(transform.position);
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //if(collision.gameObject.GetComponent<MapEditorFloor>() != null && !moved)
+        //{
+        //    if(collision.gameObject.GetComponent<MapEditorFloor>().thisFloor == FloorType.Goal)
+        //    {
+        //        Debug.Log("Goal Reached!");
+        //    }
+        //}
+
+        Vector2 normalVector = collision.contacts[0].normal;
+        Vector2 reflectVector = Vector2.Reflect(Direction, normalVector);
+        Direction = reflectVector.normalized;
+
+        #region platforms
+        if (collision.gameObject.GetComponent<MapEditorFloor>() != null && !moved)
+        {
+            //얼음 플랫폼
+            if (collision.gameObject.GetComponent<MapEditorFloor>().thisFloor == FloorType.SlipFloor)
+            {
+
+            }
+
+            //한 번 밟으면 2초 후 사라지는 플랫폼
+            if (collision.gameObject.GetComponent<MapEditorFloor>().thisFloor == FloorType.DisposableFloor)
+            {
+                Destroy(collision.gameObject, 2.0f);
+                collision.gameObject.GetComponent<Pf_Disappearing>().disappear = 1;
+            }
+
+
+
+        }
+        #endregion
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "GameOverZone" && !moved) Debug.Log("GameOver!");
+        if (collision.gameObject.CompareTag("Star"))
+        {
+            Debug.Log("star");
+            test_SerialMovement.OnTriggerstar(collision);
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.name == "CheckGoal" && !moved) Debug.Log("Goal Reached!");
+
+
+    }
+
+    public void SetStopFalse()
+    {
+        this.stop = false;
+    }
+
+    private void checkPlayerCount()
+    {
+        GameObject temp = GameObject.Find("Players");
+        List<GameObject> Players;
+        Players = new List<GameObject>();
+        Transform[] tempPlayers = temp.GetComponentsInChildren<Transform>();
+        if (tempPlayers.Length <= 2)
+        {
+            singular = true;
+        }
+    }
+}
