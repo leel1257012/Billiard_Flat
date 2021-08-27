04c9825134439757f30f8d32ee55b26d47164593
Hyunjin <hyunjin1109@korea.ac.kr>
2021-08-27 14:38:53 +0900

    Create startMenu scene, Connect startMenu and levelSelect scenes

--- Assets/Scripts/MapSelection.cs
+++ Assets/Scripts/MapSelection.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MapSelection : MonoBehaviour
@@ -35,6 +36,11 @@ public class MapSelection : MonoBehaviour
         {
             if (stage < maxStage) stage++;
             DisablePlat();
+        }
+
+        if (Input.GetKeyDown("space"))
+        {
+            SceneManager.LoadScene("LevelSelect");
         }
 
         switch(stage % 4)
